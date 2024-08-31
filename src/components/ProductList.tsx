3f8a5f7fea1737286db3b3a38130f544b6f56c72
evaniaanthony <evaniajoycelin@gmail.com>
2024-08-31 10:10:55 +0800

    try to deploy

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -14,9 +14,9 @@ const ProductList: React.FC<ProductListProps> = ({ products, onAddToCart, onProd
     return (
         <div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {products.map((product) => (
+                {products.map((product, index) => (
                     <ProductCard
-                        key={product.id}
+                        key={`${product.id}-${index}`}
                         product={product}
                         onAddToCart={onAddToCart}
                         onClick={onProductClick}
